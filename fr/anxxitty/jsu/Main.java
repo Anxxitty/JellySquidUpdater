41cf0f98b5560d64bd28e7e1ca4abc6ab8af1a7b
Anxxitty <lechartiervictor35@gmail.com>
2021-11-25 23:01:44 +0100

    Create the window

--- fr/anxxitty/jsu/Main.java
+++ fr/anxxitty/jsu/Main.java
@@ -1,7 +1,13 @@
 package fr.anxxitty.jsu;
 
+import javax.swing.JFrame;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JFrame frame = new JFrame("JellySquid Updater");
+        frame.setSize(849, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
